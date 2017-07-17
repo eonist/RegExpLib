20d54006dc290d83054f6ac6bfa5654c12288afc
Eonist <30n1st@gmail.com>
2017-07-17 23:58:10 +0200

    'Files modified: 1'
    '
    
    Modified 1 file:
    RegExp.swift
    '

--- RegExp.swift
+++ RegExp.swift
@@ -7,7 +7,6 @@ import Foundation
  * m - Control the behavior of "^" and "$" in a pattern. By default these will only match at the start and end, respectively, of the input text. If this flag is set, "^" and "$" will also match at the start and end of each line within the input text.
  * Para - Controls the behavior of \b in a pattern. If set, word boundaries are found according to the definitions of word found in Unicode UAX 29, Text Boundaries. By default, word boundaries are identified by means of a simple classification of characters as either “word” or “non-word”, which approximates traditional regular expression behavior. The results obtained with the two options can be quite different in runs of spaces and other non-word characters.
  */
-
 public class RegExp{
     /**
      * Asserts if a match exists
@@ -26,8 +25,8 @@ public class RegExp{
      * EXAMPLE: RegExp.match("hello world","(\\b\\w+\\b)")//hello, world
      * Example: RegExpParser.match("abc 123 abc 123 abc 123 xyz", "[a-zA-Z]{3}")//["abc", "abc", "abc", "xyz"]
      * TODO: ⚠️️ Probably return optional array?
-     * TODO: then if it is outof bound return eigther an empty array or nil
-     * TODO: then only do substringwithrange if NSRange is not NSOutOfBoundRange type
+     * TODO: Then if it is outof bound return eigther an empty array or nil
+     * TODO: Then only do substringwithrange if NSRange is not NSOutOfBoundRange type
      */
     static func match(_ text:String, _ pattern:String, _ options: NSRegularExpression.Options = NSRegularExpression.Options.caseInsensitive) -> [String] {
         return matches(text, pattern).map { (text as NSString).substring(with: $0.range)}
@@ -39,7 +38,7 @@ public class RegExp{
      * NOTE: use this call to get the capturing group: (str as NSString).substringWithRange(match.rangeAtIndex(1))  capturing groups from index (1 - n)
      * NOTE: use an "enum" if you need named capturing groups. like: enum FolderTaskParts:Int{ case folder = 1, content }
      * NOTE: its also possible to find number of matches this way: regex.numberOfMatchesInString(text options:[] NSMakeRange(0, nsString.length))
-     * TODO: ⚠️️Figure out how to do numbered capturing groups ($n - n is a digit. Back referencing to a capture group. n must be >= 0 and not greater than ) maybe with \$2 \$3 etc?
+     * TODO: ⚠️️ Figure out how to do numbered capturing groups ($n - n is a digit. Back referencing to a capture group. n must be >= 0 and not greater than ) maybe with \$2 \$3 etc?
      * TODO: Research how to deal with swift unicode chars, emojis etc: see this: http://stackoverflow.com/questions/25882503/how-can-i-use-nsregularexpression-on-swift-strings-with-variable-width-unicode-c
      * EXAMPLE: 
      * let str = "blue:0000FF green:00FF00 red:FF0000"
