e2042243923ff7d488f832ed0bd7dd6726213ac7
Eonist <30n1st@gmail.com>
2017-08-03 17:06:55 +0200

    'New files added: 1, Files modified: 5, Files deleted: 1'
    'Added 6 files:
    RegExp.swift
    utils/RegExpMatch.swift
    utils/RegExpModifier.swift
    utils/RegExpParser.swift
    utils/RegExpPattern.swift
    utils/RegExpAdvanceParser.swift
    
    Deleted 1 file:
    utils/RegExpHelp.swift
    
    '

--- RegExp.swift
+++ RegExp.swift
@@ -64,13 +64,13 @@ public class RegExp{
      * Replaces all matches with the replacment string
      * Returns Value A string with matching regular expressions replaced by the template string.
      * NOTE: you can use this call replaceMatchesInString to modify the original string, must use nsmutablestring to do this
-     * NOTE: NSRegularExpression. https://developer.apple.com/library/mac/documentation/Foundation/Reference/NSRegularExpression_Class/index.html
+     * NOTE: ⚠️️ NSRegularExpression has lots of good info -> https://developer.apple.com/library/mac/documentation/Foundation/Reference/NSRegularExpression_Class/index.html
+     * IMPORTANT: ⚠️️ by using "$1" or "$2" etc you can replace with the match
      * PARAM: string The string to search for values within.
      * PARAM: options: The matching options to use. See NSMatchingOptions for possible values.
      * PARAM: range: The range of the string to search.
      * PARAM: replacement: The substitution template used when replacing matching instances.
-     * TODO: The PARAM: text should be inout
-     * TODO: move to RegExpModifer, or just create RegExpUtils for all?
+     * TODO: ⚠️️ The PARAM: text should be inout, maybe?
      * EXAMPLE: RegExp.replace("<strong>Hell</strong>o, <strong>Hell</strong>o, <strong>Hell</strong>o", "<\\/?strong>",  "*")//Output:  "*Hell*o, *Hell*o, *Hell*o"
      * EXAMPLE: RegExp.replace("yeah yeah","(\\b\\w+\\b)", "bla")//bla bla
      */
@@ -85,6 +85,27 @@ public class RegExp{
             return ""
         }
     }
+    typealias Replacer = (_ match:String)->String?//if nil is returned then replacer closure didnt want to replace the match
+    /**
+     * New, replaces with a closure
+     * TODO: ⚠️️ Try to performance test if accumulative substring is faster (you += before the match + the match and so on)
+     * EXAMPLE: Swift.print("bad wolf, bad dog, Bad sheep".replace(pattern: "\\b([bB]ad)\\b"){return $0.isLowerCased ? $0 : $0.lowercased()})//bad wolf, bad dog, bad sheep
+     */
+    static func replace(_ str:String, pattern:String, options:NSRegularExpression.Options = NSRegularExpression.Options.caseInsensitive,replacer:Replacer) -> String{
+//        Swift.print("RegExp.replace")
+        var str = str
+        RegExp.matches(str, pattern).reversed().forEach() {
+            let range:NSRange = $0.rangeAt(1)
+//            Swift.print("range: " + "\(range)")
+            let stringRange:Range<String.Index> = str.stringRange(str, range.location, len: range.length)
+            let match:String = str.substring(with: stringRange)//TODO: reuse the stringRange to get the subrange here
+//            Swift.print("match: " + "\(match)")
+            if let replacment:String = replacer(match) {
+                str.replaceSubrange(stringRange, with: replacment)
+            }
+        }
+        return str
+    }
     /**
      * Extracts associated capture groups from the RegExp.matches result
      * TODO: ⚠️️ Would be great if .rawValue was done inside this method, can be done with <T> possibly look at the apple docs about enumerations
@@ -94,38 +115,64 @@ public class RegExp{
     static func value(_ str:String, _ result:NSTextCheckingResult, _ key:Int)->String{
         return (str as NSString).substring(with: result.rangeAt(key))
     }
+    /**
+     * New, finds first index of pattern in string
+     */
+    static func search(_ input:String, _ pattern:String,_ options: NSRegularExpression.Options = NSRegularExpression.Options.caseInsensitive) -> Int?{
+        guard let range = input.range(of: pattern, options:.regularExpression) else{return nil}
+        return input.distance(from:input.startIndex,to:range.lowerBound)
+    }
+    /**
+     * Coming soon
+     */
+    static func exec(){
+        //TODO: ⚠️️ research enumerateMatches, it takes a method and enumerate all matches.
+        //NSRegularExpression.replacementString has an offset, which I think you can use
+    }
+    
+//    static func replaceMatches<T: Sequence>(in source:String, matches:T, using replacer:(Match) -> String?) -> String where T.Iterator.Element : Match {
+//        
+//        "str".matches("(\\w+?)\\:([A-Z0-9]+?)(?: |$)").forEach {
+//            Swift.print("match.numberOfRanges: " + "\($0.numberOfRanges)")/*The first item is the entire match*/
+//            let content = (str as NSString).substringWithRange($0.rangeAtIndex(0))/*the entire match*/
+//            let name = $0.value("", 1)/*capturing group 1*/
+//            
+//            (str as NSString).substring(with: result.rangeAt(key))
+//            
+//        }
+//        
+//        var result = ""
+//        var lastRange:StringRange = source.startIndex ..< source.startIndex
+//        for match in matches {
+//            result += source.substring(with: lastRange.upperBound ..< match.range.lowerBound)
+//            if let replacement = replacer(match) {
+//                result += replacement
+//            } else {
+//                result += source.substring(with: match.range)
+//            }
+//            lastRange = match.range
+//        }
+//        result += source.substring(from: lastRange.upperBound)
+//        return result
+//    }
+//    
+//    
+    /**
+     Replaces all occurances of the pattern using supplied replacer function.
+     
+     - parameters:
+     - source: String to be matched to the pattern
+     - replacer: Function that takes a match and returns a replacement. If replacement is nil, the original match gets inserted instead
+     - returns: A string, where all the occurances of the pattern were replaced
+     */
+//    static func replaceAll(_ source:String,  replacer:(Match) -> String?) -> String {
+//        let matches = findAll(in: source)
+//        return replaceMatches(in: source, matches: matches, using: replacer)
+//    }
+    
 }
 extension NSTextCheckingResult{
     func value(_ str:String, _ key:Int)->String{//Convenience
         return RegExp.value(str, self, key)
     }
 }
-/*
-Research:
-
-**Wrappers:**
-http://benscheirman.com/2014/06/regex-in-swift/
-
-https://github.com/glaurent/SwiftRegExp/blob/master/RegExp.swift
-
-https://gist.github.com/ningsuhen/dc6e589be7f5a41e7794
-
-//matches:
-http://stackoverflow.com/questions/27880650/swift-extract-regex-matches
-
-//comprehensive tutorial:
-http://www.raywenderlich.com/86205/nsregularexpression-swift-tutorial
-
-//uses an infix =- as a shortcut to regexp match:
-https://github.com/kasei/SwiftRegex/blob/master/SwiftRegex/SwiftRegex.swift
-http://nomothetis.svbtle.com/clean-regular-expressions-using-conversions
-
-//Extension: has a replacer method:
-https://gist.github.com/ningsuhen/dc6e589be7f5a41e7794
-
-//find individual groups:
-http://stackoverflow.com/questions/9601551/extract-parts-from-regular-expression-with-nsregularexpression?rq=1
-
-//named capturing groups (sort of)
-http://stackoverflow.com/questions/24814974/named-capture-groups-with-nsregularexpression?lq=1
-*/
--- utils/RegExpParser.swift
+++ utils/RegExpParser.swift
@@ -1,11 +1,20 @@
 import Foundation
 
 public class RegExpParser{
-    /**
-     * Must be in the format: 12-30-1968 (mm-dd-yyyy)
-     * NOTE: works with: "12-30-1968" and "12/30/1968" syntax
-     */
-    static let usDatePattern:String = "(\\d\\d)[-\\/](\\d\\d)[-\\/](\\d\\d(?:\\d\\d)?)"//g
+    enum Pattern{
+        /**
+         * Must be in the format: 12-30-1968 (mm-dd-yyyy)
+         * NOTE: works with: "12-30-1968" and "12/30/1968" syntax
+         */
+        static let usDate:String = "(\\d\\d)[-\\/](\\d\\d)[-\\/](\\d\\d(?:\\d\\d)?)"//g
+        static let contentBetweenTitleTags:String = "(?<=<[tT][iI][tT][lL][eE]>).*(?=<\\/[tT][iI][tT][lL][eE]>)"
+        static let usPrices = "(?<=\\$)[0-9.]+"
+        static let nonUsPrices = "\\b(?<!\\$)\\d+\\b"
+        static let emails = "(?:\\w|[_.\\-])+@(?:(?:\\w|-)+\\.)+\\w{2,4}"
+        static let words = "\\b\\w+\\b"
+        static let rgbHexaDecimals = "(?<=#)[0-9A-Fa-f]{6}"
+    }
+    
     /**
      * Returns an array of computer date formated dates from PARAM: usDate
      * EXAMPLE:
@@ -15,7 +24,7 @@ public class RegExpParser{
      * NOTE: this could be made more advance by including the posibilities for single digit months and days, but that is out of the scope of this example
      */
     static func computerDate(_ usDate:String)->String {
-        let pattern:String = usDatePattern
+        let pattern:String = Pattern.usDate
         var output:String = ""
         usDate.matches(pattern).forEach {
             let month:String = $0.value(usDate, 1)
@@ -25,27 +34,170 @@ public class RegExpParser{
         }
         return output
     }
+    /**
+     * Computes and returns an array comprised of url protocol(s) from @param input
+     * @param input: a string comprised of url(s)
+     * @example:"http://www.forta.com/\n " +"https://mail.forta.com/\n " +"ftp://ftp.forta.com/\n";//http, https, ftp
+     * @Note: More url patterns in RegExpDescriber.url
+     * @Note also see RegExpParser.htmlLinks
+     */
+    static func urlProtocol(_ input:String) -> Array<String> {
+        return input.match(".+(?=:)")
+    }
+    /**
+     * Computes and returns the content between two title tags from @param input
+     * contentBetweenTitleTags("<HEAD> <TITLE>John Forta's Homepage</TITLE> </HEAD>");//John Forta's Homepage
+     * @Note: For more html parsing see RegExpMatcher and RegExpModifier
+     */
+    static func contentBetweenTitleTags(_ input:String) -> [String] {
+        return input.match(Pattern.contentBetweenTitleTags)
+    }
+    /**
+     * Computes and returns an array comprised of dollar price amounts (excluding "$" character) from @param input
+     * @example:
+     * 	var text:String =  "ABC01: $23.45 " +
+     *						"HGG42: $5.31 " +
+     *						"CFMX1: $899.00 " +
+     *						"XTC99: $69.96 " +
+     *						"Total items found: 4";
+     *	usPrices(text);//Output: 23.45,5.31,899.00,69.96
+     */
+    static func usPrices(_ input:String)-> [String] {
+        return input.match(Pattern.usPrices)
+    }
+    /**
+     * Computes and retruns an array comprised of digits not preceeded by the "$" character from @param input
+     * @example:
+     * nonUsPrices("<I paid $30 for 100 apples, 50 oranges, and 60 pears. I saved $5 on this order.");//100,50,60
+     */
+    static func nonUsPrices (_ input:String) -> [String] {
+        return input.match(Pattern.nonUsPrices);
+    }
+    /**
+     * Computes and returns lowercase words between @param rangeStart and @param rangeEnd
+     * @example:
+     */
+    static func lowerCaseWordsWithinRange(_ input:String, rangeStart:String, rangeEnd:String)-> [String] {
+        let patternString:String = "\\b[a-z]{" + rangeStart + "," + rangeEnd + "}\\b"
+        return input.match(patternString)
+    }
+    /**
+     * Computes and returns an array with lowercase words from @param input
+     * @example:
+     * getLowerCaseWordsInString("Test this out"))//this, out
+     * getLowerCaseWordsInString("test this 123"))//test, this
+     * getLowerCaseWordsInString("test this").length)//2
+     */
+    static func lowerCaseWords(_ input:String)-> [String] {
+        let pattern:String = "\\b[a-z]+\\b"//Adding i-flag here would omitt the lowercase rule
+        return input.match(pattern)
+    }
+    /**
+     * Computes and returns an array with lowercaseWords from @param input
+     */
+    static func lowerCaseWordsWithExecution(_ input:String)-> [String] {
+        let pattern:String = "\\b[a-z]+\\b"
+        return RegExp.matches(input, pattern).map {
+            Swift.print("match.numberOfRanges: " + "\($0.numberOfRanges)")/*The first item is the entire match*/
+            let word:String = $0.value(input, 1)/*capturing group 1*/
+            return word
+        }//Outputs: name: green, value: 00FF00...and so on
+    }
+    /**
+     * Returns all email instances in a string
+     * @example
+     * emails("emails: user@domain.com, user@server.com");// user@domain.com, user@server.com
+     * Tip:// simpler: \\w+@\\w+\\.\\w+
+     */
+    static func emails(_ input:String)-> [String] {
+        let pattern:String = Pattern.emails
+        return input.match(pattern)
+    }
+    /**
+     * Returns all words in a string
+     * @example:
+     * words("The German people.")//Outputs: The, German, people
+     */
+    static func words(_ input:String)-> [String] {
+        return input.match(Pattern.words)/*alternativ pattern: /[a-z]+ig/;*/
+    }
+    /**
+     * Returns all words startingWith @param firstCharacter subseeds with any of the characters passed through @param subseedingCharacters and ends with the character @param endCharacter
+     * @param startingCharacter:
+     * @param anySubseedingCharacters:
+     * @param endingCharacter:
+     * @example
+     * wordsThatStartSubseedAndEndWith("but bat samba bit nothing bet","b","aeiou","t")//Output: but,bat,bit,bet
+     */
+    static func wordsThatStartSubseedAndEndWith(_ input:String, startingCharacter:String, anySubseedingCharacters:String, endingCharacter:String)-> [String] {
+        let patternString:String = "\\b"+startingCharacter + "[" + anySubseedingCharacters + "]" + endingCharacter+"\\b";
+        return input.match(patternString);
+    }
+    /**
+     * Returns all sentences after \n ("newline break") from @param input
+     * @Note: alternative pattern: /.+/s
+     */
+    static func sentences(_ input:String)-> [String] {
+        let pattern = "^[A-z .,]"//alternative: /.+/s;//Uses the s-flag which  is:  dot matches newline
+        return input.match(pattern)
+    }
+    /**
+     * Computes and returns each word with a hexadecimal rgb value from @param input
+     * @example:
+     * rgbHexaDecimals("<BODY BGCOLOR=#336633 TEXT=#FFFFFF MARGINWIDTH=0 MARGINHEIGHT=0 TOPMARGIN=0 LEFTMARGIN=0>");//output: 336633,FFFFFF
+     */
+    static func rgbHexaDecimals(_ input:String)-> [String] {
+        return input.match(Pattern.rgbHexaDecimals)
+    }
+    /**
+     * Computes and returns each html link from @param url
+     * @param input: a string that contains an URL
+     * @example:
+     * htmlLinks("this website does searches: http://www.google.com");//http://www.google.com
+     */
+    static func htmlLinks(_ input:String) -> [String] {
+        let pattern = RegExpPattern.urlPattern
+        return input.match(pattern)
+    }
+    /**
+     * Computes and returnes the index of the first word that starts with @param startingWith from @param input
+     * @example:
+     * indexOfWordStartingWith("Something happened last winter","la");//19
+     */
+    static func indexOfWordStartingWith(_ input:String, startingWith:String) -> Int? {
+        let pattern = "\\b" + startingWith + "[a-z]"
+        return input.search(pattern)
+    }
+    /**
+     * Computes and returns U.S. ZIP codes. These may be five-digit ZIP codes formatted as 12345 or ZIP+4 codes formatted as 12345-6789. (The hyphen is used only if the additional four digits are present.)
+     * @example:
+     * usZipCodes("11111 22222 33333- 44444-4444");
+     */
+    static func usZipCodes(input:String)-> [String] {
+        var pattern:String = ""
+            pattern += "\\d{5}" //5 digits
+            pattern += "(" //Group1 start
+            pattern += "?" //if the subseeding condition is met
+            pattern += "(" //Group2 start
+            pattern += "?=-" //Math "-" if it exists (but not consume)
+            pattern += ")" //Group2 end
+            pattern += "-" //Subseeded by "-" (if group2 condition is met)
+            pattern += "\\d{4}" //Subseeded by 4 digits (if group2 condition is met)
+        pattern += ")"//Group1 end
+        return input.match(pattern)
+    }
+    /**
+     * Computes and retuns an array comprised of objects containing a name and a value from @param input
+     * @param input a string containing css properties
+     * @example cssProoperties("color:blue;thickness:2;font:Helvetica;");//[{name:color,value:blue},{name:thickness,value:2},{name:font,value:helvetica}]
+     */
+    static func cssProperties(_ input:String)-> [(name:String,value:String)] {
+        let pattern = "(\\w*?)\\:(.*?)\\;"//assigns the name and value to an object (Associative) // :TODO: (the dot in the end part could possibly be replaced by [.^\;] test this)
+        return input.matches(pattern).map {//Loops through the pattern
+            let name = $0.value(input, 1)/*capturing group 1*/
+            let value = $0.value(input, 2)/*capturing group 2*/
+            return (name:name,value:value)
+        }
+    }
 }
-/*
-	Add these from legacy:
-	match,
-	associatedMatch,
-	urlProtocol,
-	contentBetweenTitleTags,
-	usPrices,
-	nonUsPrices,
-	lowerCaseWordsWithinRange,
-	lowerCaseWords,
-	lowerCaseWordsWithExecution,
-	emails,
-	words,
-	wordsThatStartSubseedAndEndWith,
-	sentences,
-	rgbHexaDecimals,
-	computerDate,
-	htmlLinks,
-	indexOfWordStartingWith,
-	split,
-	usZipCodes,
-	cssProperties;/*Great example off associative and array use*/
-	*/
+
